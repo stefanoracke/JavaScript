c9dc4b83a0ab877f8ee43236d023200407cfb062
stefanoracke <stefanoracke@gmail.com>
2022-03-22 09:47:38 -0300

    first commit

---
+++ aed.py
@@ -0,0 +1,4 @@
+a = int(input("ingrese un numero: "))
+b = int(input("ingrese el segundo numero: "))
+
+print(f"El resultado es: {a//b} \ny el resto es {a%b}")
---
+++ app.js
@@ -0,0 +1,627 @@
+var works = [
+    {
+      id: 0,
+      name: 'Bienaventuranza',
+      time: '2 min',
+      language: 'Spanish',
+      thematic: 'Sacral',
+      year: '2006',
+      editorial: 'http://www.goldberg-verlag.com/en/catalogue/bienaventuranza/',
+      media: 'https://www.youtube.com/c/edgardmoyagodoymusic',
+      img: '/assets/img/0.png',
+      price: 4.99,
+    },
+    {
+      id: 1,
+      name: 'Kyrie Eleison',
+      time: '2 1/2 min',
+      language: 'Greek',
+      thematic: 'Sacral',
+      year: '2008',
+      editorial: 'http://www.goldberg-verlag.com/en/catalogue/kyrie/',
+      media: 'https://open.spotify.com/track/3Qt9o2VjbByXsy2Lvnkq1F?si=iV-71m0MSaqM6eVMWqlDWQ',
+      img: '/assets/img/1.png',
+      price: 2.99,
+    },
+    {
+      id: 2,
+      name: 'Stabat Mater',
+    time: '2 1/2 min',
+    language: 'Latin',
+    thematic: 'Sacral',
+    year: '2008',
+    editorial:'/#/contact',
+    media:'https://open.spotify.com/track/14am69wwXh8BtsWUvqxcoU?si=KLZBGK4SRP-Q7HxmybV5xQ',
+    img:'/assets/img/2.png',
+    price: 3.99,
+  },
+  {
+    id: 3,
+    name: 'Gloria',
+    time: '2 1/2 min',
+    language: 'Latin',
+    thematic: 'Sacral',
+    year: '2008',
+    editorial: '/#/contact',
+    media: 'https://open.spotify.com/track/5UR3NAtRfo8JKWFJqR8o2B?si=5G8iW7A8Q-G0U3-kiulg1Q',
+    img:'assets/img/3.png',
+    price: 4.99,
+  },
+  {
+    id:4,
+    name: 'Linde espiral',
+    time: '4 1/2 min',
+    language: 'Spanish',
+    thematic: 'Love',
+    year: '2010',
+    editorial: '/#/contact',
+    media: 'https://www.youtube.com/c/edgardmoyagodoymusic',
+    img:'assets/img/4.png',
+    price: 4.99,
+},
+  {
+    id:5,
+    name: 'Contemplaciones ',
+    time: '11 min',
+    language: 'Spanish',
+    thematic: 'Abstracta ',
+    year: '2009-2012',
+    editorial: '/#/contact/',
+    media:'https://soundcloud.com/edgard-moya-godoy/contemplaciones-primeras',
+    img:'assets/img/5.png',
+    price: 4.99,
+  },
+  {
+    id:6,
+    name: 'Salmo 46 ',
+    time: '6,5 min',
+    language: 'Latin',
+    thematic: 'Sacra ',
+    year: '2012',
+    editorial: 'http://www.goldberg-verlag.com/en/catalogue/salmo-46/',
+    media:'https://www.youtube.com/watch?v=f5OfKb9UzDU',
+    img:'assets/img/6.png',
+    price: 4.99,
+  },
+  {
+    id: 7,
+    name: 'O bone Jesu',
+  time: '4 min',
+  language: 'Latin',
+  thematic: 'Sacra ',
+  year: '2013',
+  editorial:'/#/contact',
+  media:'https://www.youtube.com/watch?v=YAqy4MukzZ8',
+  img:'assets/img/7.png',
+  price: 4.99,
+},
+{
+  id: 8,
+  name: 'Zamba Vacía',
+  time: '4,5 min',
+  language: 'Spanish',
+  thematic: 'Melancholic',
+  year: '2013',
+  editorial:'/#/contact',
+  media:'https://www.youtube.com/c/edgardmoyagodoymusic',
+  img:'assets/img/8.png',
+  price: 4.99,
+},
+{
+  id: 9,
+  name: 'Letanía I',
+  time: '7 min',
+  language: 'Spanish',
+  thematic: 'Melancholic',
+  year: '2013',
+  editorial:'/#/contact',
+  media:'https://www.youtube.com/c/edgardmoyagodoymusic',
+  img:'assets/img/9.png',
+  price: 4.99,
+},
+{
+  id: 10,
+  name: 'Canticle I – (…the rose, the lily…) ',
+  time: '5 min',
+  language: 'English',
+  thematic: 'Love',
+  year: '2014',
+  editorial:'/#/contact',
+  media:'https://www.youtube.com/c/edgardmoyagodoymusic',
+  img:'assets/img/10.png',
+  price: 4.99,
+},
+{
+  id: 11,
+  name: 'Desenlace',
+  time: '4 min',
+  language: 'Spanish',
+  thematic: 'Thoughtful',
+  year: '2014',
+  editorial:'http://www.goldberg-verlag.com/en/catalogue/desenlace/',
+  media:'https://www.youtube.com/watch?v=DtVxRfahJus',
+  img: 'assets/img/11.png',
+  price: 4.99,
+},
+{
+  id: 12,
+  name: 'Dominus Deus',
+  time: '4 min',
+  language: 'Spanish + latin',
+  thematic: 'Sacral',
+  year: '2015',
+  editorial:'http://www.goldberg-verlag.com/en/catalogue/dominus-deus/',
+  media:'https://www.youtube.com/c/edgardmoyagodoymusic',
+  img: 'assets/img/12.png',
+  price: 4.99,
+},
+{
+  id: 13,
+  name: 'In God I have put my trust ',
+  time: '4 min',
+  language: 'English',
+  thematic: 'Sacral',
+  year: '2016',
+  editorial:'/#/contact',
+  media:'https://www.youtube.com/c/edgardmoyagodoymusic',
+  img: 'assets/img/13.png',
+  price: 4.99,
+},
+{
+  id: 14,
+  name: 'El Cusifai',
+  time: '7 min',
+  language: 'Spanish',
+  thematic: 'Abstract',
+  year: '2016',
+  editorial:'/#/contact',
+  media:'https://www.youtube.com/c/edgardmoyagodoymusic',
+  img: 'assets/img/14.png',
+  price: 4.99,
+},
+{
+  id: 15,
+  name: 'Verbo Luz',
+  time: '6 min',
+  language: 'Spanish',
+  thematic: 'Sacral',
+  year: '2018',
+  editorial:'/#/contact',
+  media:'https://www.youtube.com/c/edgardmoyagodoymusic',
+  img: 'assets/img/15.png',
+  price: 4.99,
+},
+{
+  id: 16,
+  name: 'Nos ha nacido Emmanuel',
+  time: '2 min',
+  language: 'Spanish',
+  thematic: 'Sacral',
+  year: '2018',
+  editorial:'/#/contact',
+  media:'https://www.youtube.com/c/edgardmoyagodoymusic',
+  img: 'assets/img/16.png',
+  price: 4.99,
+},
+{
+  id: 17,
+  name: 'Cánticos del Evangelio según San Juan ',
+  time: '70 min',
+  language: 'Spanish',
+  thematic: 'Sacral',
+  year: '2018',
+  editorial:'/#/contact',
+  media:'https://www.youtube.com/watch?v=Pt__AjYmZKg&list=PLAY26fzD_QGXPEArCzUWyqjOty0TZzCkz',
+  img: 'assets/img/17.png',
+  price: 4.99,
+},
+{
+  id: 18,
+  name: 'Copla Libre',
+  time: '4 min',
+  language: 'Spanish',
+  thematic: 'Fraternity',
+  year: '2020',
+  editorial:'/#/contact',
+  media:'https://www.youtube.com/c/edgardmoyagodoymusic',
+  img:'assets/img/18.png',
+  price: 4.99,
+},
+{
+  id: 19,
+  name: 'Rima de la Mudez y los Gritos (además de la memoria) ',
+  time: '27 min',
+  language: 'Spanish',
+  thematic: 'Abstract',
+  year: '2020',
+  editorial:'/#/contact',
+  media:'https://www.youtube.com/c/edgardmoyagodoymusic',
+  img: 'assets/img/19.png',
+  price: 4.99,
+},
+{
+  id: 20,
+  name: 'Canticos del evangelio según San Juan 2da Parte ',
+  time: '50 min',
+  language: 'Spanish',
+  thematic: 'Sacral',
+  year: '2021',
+  editorial:'/#/contact',
+  media:'https://www.youtube.com/c/edgardmoyagodoymusic',
+  img:'assets/img/20.png',
+  price:  4.99,
+},
+  ]
+
+//variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn =document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM =document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal= document.querySelector(".cart-total");
+const cartContent= document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+ 
+
+//cart
+let cart = [];
+
+let infos = [];
+//buttons
+
+let buttonsDOM = [];
+
+//getting the products
+class Products{
+    async getProducts(){
+        fetch('works.json')
+    }
+}
+
+function setCartValues(cart){
+  let tempTotal = 0;
+  let itemsTotal = 0;
+
+  
+
+  cart.forEach(item =>{
+    tempTotal += item.price * item.amount;
+    itemsTotal += item.amount;
+  });
+    
+  
+  cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+  cartItems.innerText = itemsTotal;
+  
+}
+
+function addCartItem(item){
+  const div = document.createElement('div');
+  div.classList.add('cart-item');
+  div.innerHTML = ` <img src='${item.img}' alt="product">
+  <div>
+      <h4>${item.name}</h4>
+      <h5>${item.price}</h5>
+      <span class="remove-item" data-id=${item.id}>remove</span>
+  </div>
+  <div>
+      <i class="fas fa-chevron-up" data-id=${item.id}></i>
+      <p class="item-amount">${item.amount}</p>
+      <i class="fas fa-chevron-down" data-id=${item.id}></i>
+  </div>`;
+
+  cartContent.appendChild(div);
+  
+}
+
+function showCart(){
+  cartOverlay.classList.add('transparentBcg');
+  cartDOM.classList.add('showCart');
+}
+
+function showInfo(supe){
+  supe.classList.add('showInfo');
+  
+  
+}
+
+function closeInfo(supe){
+  supe.classList.remove('showInfo');
+  
+}
+
+function openCart(){
+  let f=0;
+  
+  cartBtn.addEventListener('click',event => {
+    
+    if(f===0){
+      showCart();
+      f=1;
+    }else{
+     hideCart();
+     f=0;
+    }
+  });
+  
+}
+
+function hideCart(){
+  cartOverlay.classList.remove('transparentBcg');
+  cartDOM.classList.remove('showCart');
+}
+
+function getSingleItem(cart, id){
+  return  cart.find(element => element.id === id); 
+}
+
+//display products
+class UI {
+
+  displayWorks(works){
+    let result = '';
+    works.forEach(work =>{
+      result += `
+      <article class="product">
+                <div class="img-container">
+                <div class="info">
+                <div class="data-info">
+                    <button class="close-info"><i class="fas fa-window-close" ></i></button>
+                    <h5>Year: ${work.year} </h5>
+                    <h5>Thematic: ${work.thematic}</h5>
+                    <h5>Length: ${work.time}</h5>
+                    <h5>Language: ${work.language}</h5>
+                    <br>
+                    <button class="media"><a class="listen" href="${work.media}"><i class="fas fa-music"> Listen!</i></a></button>
+                </div>
+            </div>
+                    <img src=${work.img} alt="product" class="product-img">
+                    <button class="bag-btn" data-id="${work.id}">
+                        <i class="fas fa-shopping-cart"></i>
+                        add to cart
+                    </button>
+                    <button class="info-btn">
+                        <i class="fas fa-info-circle"></i>
+                        Info
+                    </button>
+                </div>
+                <h3>${work.name}</h3>
+                <h4>$${work.price}</h4>
+            </article>
+      `;
+    });
+    productsDOM.innerHTML = result;
+  }
+  getBagButtons(){
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    
+    
+    buttonsDOM = buttons;
+
+    
+      buttons.forEach(function callback(button,index){
+        let id = index;
+        
+        let inCart = cart.find(item => item.id === id );
+        if(inCart){
+          button.innerText = "In Cart";
+          button.disabled = true;
+          
+        }
+        
+        button.addEventListener('click',(event)=>{
+          event.target.innerText = "In Cart";
+          event.target.disabled = true;
+            // get produts from product   
+          let cartItem = {...Storage.getProduct(id),amount:1};
+
+          
+          
+            //add product to the cart
+          cart = [...cart,cartItem];
+          
+            //save cart in local storage
+          
+          Storage.saveCart(cart);
+            //set cart values
+          
+          cart = Storage.getCart();
+          Storage.saveCart(cart);
+          
+          setCartValues(cart);
+            //display cart items
+          addCartItem(cartItem);
+            //show the cart
+          showCart();
+          });    
+      });
+      openCart();
+  }
+
+  getButtonCloseCart(button){
+    button.addEventListener('click',(event)=>{
+      hideCart();
+    })
+  }
+
+  getInfoButtons(){
+    const infobtn = [...document.querySelectorAll(".info-btn")];
+    const info = [...document.querySelectorAll(".info")];
+    const closeinfobtn = [...document.querySelectorAll(".close-info")];
+
+    infobtn.forEach(function callback(button,index){
+      let id = index;
+      
+      let inCart = cart.find(item => item.id === id );
+      if(inCart){ 
+      }
+      let f= 0; 
+      button.addEventListener('click',(event)=>{
+        
+        
+         // get produts from product   
+        if(f===0){
+          showInfo(info[id]);
+          f=1;
+          
+        }else if(f===1){
+          closeInfo(info[id]);
+          f=0;
+        }        
+
+        });    
+    });
+
+    closeinfobtn.forEach(function callback(button,index){
+      let id = index;
+      
+      let inCart = cart.find(item => item.id === id );
+      if(inCart){ 
+      }
+      
+      button.addEventListener('click',(event)=>{
+        
+          // get produts from product   
+        closeInfo(info[id]);
+        
+        });    
+    });
+
+
+  }
+
+ 
+
+  clearCart(){
+    let cartItems = cart.map(item => item.id);
+    cartItems.forEach(id => this.removeItem(id));
+    while(cartContent.children.length>0){
+      cartContent.removeChild(cartContent.children[0])
+    }
+  }
+
+  removeItem(id){
+    cart = cart.filter(item => item.id !== id);
+    setCartValues(cart);
+    Storage.saveCart(cart);
+    let button = this.getSingleButton(id);
+    button.disabled = false
+    button.innerHTML = `<i class="fas fa-shopping-cart"></i>add to cart`;
+  }
+
+  getSingleButton(id){
+    return buttonsDOM[id]
+
+  }
+
+  //cart functionality
+
+  //end cart functionality
+
+  cartLogic() {
+    clearCartBtn.addEventListener('click',()=>{
+      this.clearCart();
+    });
+    //cart functionality
+    cartContent.addEventListener('click', event=>{
+      if(event.target.classList.contains('remove-item')){
+        let removeItem = event.target;
+        let id = removeItem.dataset.id;
+        
+        cartContent.removeChild(removeItem.parentElement.parentElement);
+        this.removeItem(id)
+      }else   if(event.target.classList.contains('fa-chevron-up')){
+        let up = event.target;
+        let id = up.dataset.id;
+        
+        
+        let singleItem = cart.find(item => item.id == id);        
+        
+        singleItem.amount += 1;
+        Storage.saveCart(cart);
+        setCartValues(cart);
+        up.nextElementSibling.innerText = singleItem.amount;
+      }else if(event.target.classList.contains('fa-chevron-down')){
+        let down = event.target;
+        let id = down.dataset.id
+        let tempItem = cart.find(item => item.id == id);
+
+        tempItem.amount -= 1;
+        
+        if(tempItem.amount >0){
+          Storage.saveCart(cart);
+          setCartValues(cart);
+          down.previousElementSibling.innerText =tempItem.amount;
+        }else{
+          cartContent.removeChild(down.parentElement.parentElement);
+          this.removeItem(id)
+        }
+        
+      }
+    })
+    
+  }
+  
+  getCarOnReload(){
+    cart = Storage.getCart();
+    Storage.saveCart(cart);
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttonsDOM = buttons;
+    setCartValues(cart);
+    
+
+    cart.forEach(function callback(elemnt,id){
+      let inCart = cart.find(item => item.id == id);
+      addCartItem(elemnt);
+
+    })
+    
+  }
+}
+
+//localstorage
+class Storage{
+  static saveProducts(works){
+    localStorage.setItem("works", JSON.stringify(works));
+  };
+
+  static getProduct(id){
+    let products = JSON.parse(localStorage.getItem('works'));
+    return products[id]
+  };
+
+  static saveCart(cart){
+    localStorage.setItem("cart", JSON.stringify(cart));
+    
+  };
+
+  static getCart(){
+    return localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart"))
+    : [];
+  }
+  
+}
+
+document.addEventListener('DOMContentLoaded',function(){
+    
+
+    const ui = new UI();
+    const obras = works;
+    
+    
+    ui.displayWorks(works);
+
+    Storage.saveProducts(works);
+    ui.getCarOnReload();
+    ui.getBagButtons();
+    ui.getButtonCloseCart(closeCartBtn);
+    ui.getInfoButtons();
+    
+    ui.cartLogic();
+
+    
+})
